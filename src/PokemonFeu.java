7055b255cacbed242ec5162e17019e19a137740e
Abder-Rahmane <you@example.com>
2021-09-30 14:18:53 +0200

    mise a jour pokemonFeu

--- src/PokemonFeu.java
+++ src/PokemonFeu.java
@@ -1,2 +1,28 @@
-public class PokemonFeu {
+public class PokemonFeu extends Pokemon{
+
+    public Pokemon PokemonEau;
+    public Pokemon Pokemon;
+
+    public PokemonFeu(String nom, int hp, int atk){
+        super (nom,hp,atk);
+
+    }
+
+    @Override
+    public void attacker (Pokemon p) {
+        if (p == PokemonEau) {
+            p.hp = atk * 2 - p.hp;
+        } else
+            p.hp = (int) (atk * 0.5 - p.hp);
+        if (p == Pokemon) {
+            p.hp= atk - p.hp;
+        }
+
+    }
+
+
+
+
+
+}
 }
