df39725e495e1cc69a596a2ec4a38b636f6f4d39
Abder-Rahmane <90828091+Abder-Rahmane@users.noreply.github.com>
2021-09-30 23:04:36 +0200

    Mise a jour du pokemon

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,12 +1,45 @@
-public class Pokemon {
+public class Pokemon{
 
-    public String nom;
-    public int hp ;
-    public String atk ;
+    protected Pokemon p;
+    protected Pokemon p ;
 
-    public Pokemon(String nom, int hp, String atk) {
+    protected String nom;
+    protected double hp;
+    protected int atk;
+
+    public Pokemon(String nom,int hp, int atk) {
         this.nom = nom;
         this.hp = hp;
         this.atk = atk;
+
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public double gethp() {
+        return hp;
+    }
+    public int getAtk() {
+        return atk;
+    }
+    public boolean isDead(){
+        if (this.hp == 0){
+            return true;}
+        else
+            return false;
+    }
+    public void attacker(Pokemon p){
+
+        p.hp = this.atk - p.hp;
+
+    }
+    public void toString(String nom,int hp, int atk) {
+
+        System.out.println(this.nom);
+        System.out.println(this.hp);
+        System.out.println(this.atk);
+
     }
 }
