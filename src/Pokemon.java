dfa5b6f54b825b3b64e7c8c5728cc93c1f2a9154
Your Name <you@example.com>
2021-09-30 11:28:37 +0200

    Création class pokemon (atribut & constructeur)

---
+++ src/Pokemon.java
@@ -0,0 +1,12 @@
+public class Pokemon {
+
+    public String nom;
+    public int hp ;
+    public String atk ;
+
+    public Pokemon(String nom, int hp, String atk) {
+        this.nom = nom;
+        this.hp = hp;
+        this.atk = atk;
+    }
+}
