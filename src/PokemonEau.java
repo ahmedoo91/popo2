12e47b2dd1c02b2fd205ae969c928c8319a5818f
Abder-Rahmane <you@example.com>
2021-09-30 14:20:50 +0200

    Mise a jour PokemonEau

--- src/PokemonEau.java
+++ src/PokemonEau.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class PokemonEau {
+public class PokemonEau extends Pokemon{
+
+    public Pokemon PokemonEau;
+    public Pokemon Pokemon;
+
+    public PokemonEau(String nom, int hp, int atk){
+        super (nom,hp,atk);
+
+    }
+
+    @Override
+    public void attacker (Pokemon p) {
+        if (p == PokemonEau) {
+            p.hp = atk * 2 - p.hp;
+        } else
+            p.hp = (int) (atk * 0.5 - p.hp);
+        if (p == Pokemon) {
+            p.hp= atk - p.hp;
+        }
+
+    }
+
+
+
+
+
+}
 }
