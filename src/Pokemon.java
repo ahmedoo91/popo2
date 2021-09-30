af0f89e32d8ee3b4c361b7f445c98eb89f5a3097
Abder-Rahmane <you@example.com>
2021-09-30 13:47:39 +0200

    Modification V2 du pokemon.java

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,12 +1,42 @@
-public class Pokemon {
+public class Pokemon{
+    protected String nom;
+    protected int hp;
+    protected int atk;
 
-    public String nom;
-    public int hp ;
-    public String atk ;
-
-    public Pokemon(String nom, int hp, String atk) {
+    public Pokemon(String nom,int HP, int atk) {
         this.nom = nom;
-        this.hp = hp;
+        this.hp = HP;
         this.atk = atk;
+
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int gethp() {
+        return hp;
+    }
+
+    public int getAtk() {
+        return atk;
+    }
+    public boolean isDead(){
+        if (this.hp == 0){
+            return true;}
+        else
+            return false;
+    }
+    public void attacker(Pokemon p){
+
+        p.hp = this.atk - p.hp;
+
+    }
+    public void toString(String nom,int hp, int atk) {
+
+        System.out.println(this.nom);
+        System.out.println(this.hp);
+        System.out.println(this.atk);
+
     }
 }
