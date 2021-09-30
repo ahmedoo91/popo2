b23387b6520febd2c74d6c07abbbf2537c0a0c3d
Abder-Rahmane <90828091+Abder-Rahmane@users.noreply.github.com>
2021-09-30 23:07:04 +0200

    Mise a jour PokemonEau

---
+++ src/PokemonEau.java
@@ -0,0 +1,27 @@
+public class PokemonEau extends Pokemon {
+
+
+
+    public PokemonEau(String nom, int hp, int atk) {
+        super(nom, hp, atk);
+    }
+
+    @Override
+    public void attacker(Pokemon p) {
+        if (p = PokemonFeu) {
+
+            p.hp = this.atk * 2 - p.hp;
+
+        }
+        else if (p = PokemonPlante) {
+
+            p.hp = this.atk * 0.5 - p.hp;
+        }
+        else
+            p.hp = this.atk - p.hp;
+
+
+
+    }
+
+}
