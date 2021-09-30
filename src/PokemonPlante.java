4c612b846d21835cf5187d2e136ae75ea881fd4e
Your Name <you@example.com>
2021-09-30 14:16:51 +0200

    mise a jour pokemonplante

--- src/PokemonPlante.java
+++ src/PokemonPlante.java
@@ -1,2 +1,28 @@
-public class gPokemonPlante {
+public class PokemonPlante extends Pokemon{
+
+    public Pokemon PokemonEau;
+    public Pokemon Pokemon;
+
+    public PokemonPlante(String nom, int hp, int atk){
+            super (nom,hp,atk);
+
+        }
+
+        @Override
+        public void attacker (Pokemon p) {
+            if (p == PokemonEau) {
+                p.hp = atk * 2 - p.hp;
+            } else
+                p.hp = (int) (atk * 0.5 - p.hp);
+            if (p == Pokemon) {
+                p.hp= atk - p.hp;
+            }
+
+        }
+
+
+
+
+
+}
 }
